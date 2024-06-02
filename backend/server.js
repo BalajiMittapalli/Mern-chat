052114b25b4354db545fb1a10f5b7ab58a4792ee
BalajiMittapalli <mittapallibalaji8@gmail.com>
2024-06-02 09:17:46 +0530

    final deploy

--- backend/server.js
+++ backend/server.js
@@ -17,13 +17,17 @@ dotenv.config();
 
 const PORT=process.env.PORT || 5000;
 
+const __dirname =path.resolve();
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
-
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+});
 server.listen(PORT,()=> {
     connectToMongoDB();
     console.log(`Server running on port ${PORT}`)}
